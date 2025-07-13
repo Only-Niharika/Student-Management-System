2f569bdf2a308c9c1ed71d99448bfa7eb2c2513a
Niharika Karkra <itzz.tiya10@gmail.com>
2025-07-13 17:33:02 +0530

    style file

--- header.php
+++ header.php
@@ -1,2 +1,19 @@
-<html>
-</html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <header>
+        <div class="header">
+        <div class="logo">
+            <img src="image/logo.png" alt="logo" height="90px" width="90px">
+        </div>
+        <div class="header-links">
+            <ul>
+                <li><a href="http://localhost/Project(Github)/Student-Management-System/home">Home</a></li>
+                <li><a href="http://localhost/Project(Github)/Student-Management-System/about">About Us</a></li>
+                <li><a href="http://localhost/Project(Github)/Student-Management-System/contact">Contact Us</a></li>
+            </ul>
+        </div>
+        </div>
+    </header>
