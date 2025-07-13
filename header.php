f94edb7aeb6218768a0761332127e7cd0e28893f
Niharika Karkra <itzz.tiya10@gmail.com>
2025-07-13 19:20:34 +0530

    Header complete

--- header.php
+++ header.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
         <div class="header">
-        <div class="logo">
-            <img src="image/logo.png" alt="logo" height="90px" width="90px">
-        </div>
-        <div class="header-links">
-            <ul>
-                <li><a href="http://localhost/Project(Github)/Student-Management-System/home">Home</a></li>
-                <li><a href="http://localhost/Project(Github)/Student-Management-System/about">About Us</a></li>
-                <li><a href="http://localhost/Project(Github)/Student-Management-System/contact">Contact Us</a></li>
-            </ul>
-        </div>
+            <div class="logo">
+                <img src="image/logo.png" alt="logo" height="90" width="90">
+            </div>
+            <nav class="header-links">
+                <ul>
+                    <li><a href="http://localhost/Project(Github)/Student-Management-System/home">Home</a></li>
+                    <li><a href="http://localhost/Project(Github)/Student-Management-System/about">About Us</a></li>
+                    <li><a href="http://localhost/Project(Github)/Student-Management-System/contact">Contact Us</a></li>
+                    <li><a href="http://localhost/Project(Github)/Student-Management-System/home">Login</a></li>
+                </ul>
+            </nav>
         </div>
     </header>
