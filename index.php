25de3d8e002cce9ed851889236d672e2ea85cdc8
Only-Niharika <niharikakarkra06@gmail.com>
2025-07-21 22:55:17 +0530

    Today's Work

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div>
+        <?php include_once('header.php'); ?>
+    </div>
+
+    <div class="hero_section">
+        <!-- Optional: Hero content goes here -->
+    </div>
+</body>
+</html>
