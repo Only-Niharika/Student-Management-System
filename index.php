d1fd844881963ae69a7fe81940c3d5b319039209
Only-Niharika <niharikakarkra06@gmail.com>
2025-07-25 00:56:10 +0530

    Section Added

--- index.php
+++ index.php
@@ -12,8 +12,35 @@
     <div class="hero_section">
         <div class="overlay"></div>
         <div class="content">
-            <h1>Welcome to Our School</h1>
-            <p>Building the future one student at a time.</p>
+            <h1 style="font-size:40px;">Welcome to Our School</h1>
+            <p style="font-size:20px;">A World of Learning in Every Classroom.</p>
+        </div>
+    </div>
+
+    <div class="Section_1">
+        <span>Features</span>
+        <h2>One Platform Many Course </h2>
+        <div class="icon_box">
+        <div class="icon_box_1">
+            <img src="image/man.png" height="60px" width="60px">
+            <h3>Exclusive Advisor</h3>
+            <p>Lorem ipsum dolor amet consectetur</p>
+        </div>
+        <div class="icon_box_1">
+            <img src="image/man.png" height="60px" width="60px">
+            <h3>Exclusive Advisor</h3>
+            <p>Lorem ipsum dolor amet consectetur</p>
+        </div>
+        <div class="icon_box_1">
+            <img src="image/man.png" height="60px" width="60px">
+            <h3>Exclusive Advisor</h3>
+            <p>Lorem ipsum dolor amet consectetur</p>
+        </div>
+        <div class="icon_box_1">
+            <img src="image/man.png" height="60px" width="60px">
+            <h3>Exclusive Advisor</h3>
+            <p>Lorem ipsum dolor amet consectetur</p>
+        </div>
         </div>
     </div>
 </body>
