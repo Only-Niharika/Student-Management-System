bf1df29ce43672d32797bf01e01d6ccc7cb47f3d
Only-Niharika <niharikakarkra06@gmail.com>
2025-08-11 16:36:18 +0530

    Initial commit

---
+++ header.php
@@ -0,0 +1,14 @@
+<div class="header">
+    <div class="logo">
+        <img src="image/logo.png" alt="logo" height="90" width="90">
+    </div>
+    <nav class="header-links">
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="about">About Us</a></li>
+            <li><a href="contact">Contact Us</a></li>
+            <li><a href="register">Register</a></li>
+            <li><a href="login">Login</a></li>
+        </ul>
+    </nav>
+</div>
---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Student Management System</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        <?php include_once('header.php'); ?>
+    </header>
+
+    <div class="hero_section">
+        <div class="overlay"></div>
+        <div class="content">
+            <h1 style="font-size:40px;">Welcome to Our School</h1>
+            <p style="font-size:20px;">A World of Learning in Every Classroom.</p>
+        </div>
+    </div>
+
+    <div class="Section_1">
+        <span>Features</span>
+        <h2>One Platform Many Course </h2>
+        <div class="icon_box">
+        <div class="icon_box_1">
+            <img src="image/man.png" height="60px" width="60px">
+            <h3>Exclusive Advisor</h3>
+            <p>Lorem ipsum dolor amet consectetur</p>
+        </div>
+        <div class="icon_box_1">
+            <img src="image/man.png" height="60px" width="60px">
+            <h3>Exclusive Advisor</h3>
+            <p>Lorem ipsum dolor amet consectetur</p>
+        </div>
+        <div class="icon_box_1">
+            <img src="image/man.png" height="60px" width="60px">
+            <h3>Exclusive Advisor</h3>
+            <p>Lorem ipsum dolor amet consectetur</p>
+        </div>
+        <div class="icon_box_1">
+            <img src="image/man.png" height="60px" width="60px">
+            <h3>Exclusive Advisor</h3>
+            <p>Lorem ipsum dolor amet consectetur</p>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
